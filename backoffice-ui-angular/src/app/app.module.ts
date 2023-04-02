74c3f97bfefe36427f10feaca35ca2851b9e7065
Christian Luzzetti <christian.luzzetti@gmail.com>
2023-04-02 16:08:10 +0200

    added inner styling of primeNG components

--- backoffice-ui-angular/src/app/app.module.ts
+++ backoffice-ui-angular/src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RestaurantComponent } from './restaurant/restaurant.component';
-import { RestaurantIntroCardComponent } from './restaurant/restaurant-intro-card/restaurant-intro-card.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RestaurantComponent} from './restaurant/restaurant.component';
+import {
+  RestaurantIntroCardComponent
+} from './restaurant/restaurant-intro-card/restaurant-intro-card.component';
 import {CardModule} from "primeng/card";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 import {MenubarModule} from "primeng/menubar";
 import {AvatarModule} from "primeng/avatar";
 import {RatingModule} from "primeng/rating";
 import {FormsModule} from "@angular/forms";
+import {DividerModule} from "primeng/divider";
+import {AccordionModule} from "primeng/accordion";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {PanelModule} from "primeng/panel";
 
 @NgModule({
   declarations: [
@@ -23,14 +29,19 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CardModule,
     MenubarModule,
     AvatarModule,
     RatingModule,
-    FormsModule
+    FormsModule,
+    DividerModule,
+    AccordionModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
