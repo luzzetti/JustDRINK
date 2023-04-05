1cfbf9e5dfcc42a07fd024ddbdd7d030bf0d41e6
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-04-05 17:32:06 +0200

    chimata in post products

--- backoffice-ui-vue/src/pages/ShowProduct.vue
+++ backoffice-ui-vue/src/pages/ShowProduct.vue
@@ -1,11 +1,14 @@
 <template>
-   <h1>Aggiungi Prodotti</h1> 
+    <h1>Aggiungi Prodotti</h1>
 
     <div>
         <form action="" method="POST">
             <div class="d-flex flex-column w-25 mt-4">
                 <h2 class="mt-2" for="">Inserisci nome prodotto</h2>
-                <input type="text w-25" placeholder="inserisci prodotto">
+                <label for="">Nome</label>
+                <input type="text w-25" v-model="name" placeholder="inserisci nome prodotto">
+                <label for="">Prezzo</label>
+                <input type="number w-25" v-model="price" placeholder="inserisci prezzo prodotto">
                 <button class="btn btn-secondary mt-2" @click.prevent="InviaProdotto()">Crea prodotto</button>
             </div>
         </form>
@@ -15,30 +18,30 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
-    export default {
-        name:'ShowProduct',
-        data() {
+export default {
+    name: 'ShowProduct',
+    data() {
         return {
-           product: '',
-            sections: [],
+            name: '',
+            price: '',
+            products: [],
         }
     },
     methods: {
 
         InviaProdotto() {
             let newData = {
-                product: this.product
+                name: this.name,
+                price: this.price
             }
-            axios.post(`${store.apiBaseUrl}/api/1.0/restaurants/${this.$route.params.id}/menu/sections/${this.$route.params.id}/products`, newData).then((response) => {
+            axios.post(`${store.apiBaseUrl}/api/1.0/restaurants/${this.$route.params.restaurantId}/menu/sections/${this.$route.params.sectionId}/products`, newData).then((response) => {
                 console.log(response.data);
-                this.product = '';
-                
+                this.name = '';
+                this.price='';
             })
         },
     }
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
