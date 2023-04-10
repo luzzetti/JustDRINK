8c83849892c279d3bf3bdf3f6f01c0e561ba7a09
Christian Luzzetti <christian.luzzetti@gmail.com>
2023-04-10 15:23:26 +0200

    Fixed polymorphic apis for business hours

--- backoffice-service/backoffice-infrastructure/backoffice-input-rest-adapter/src/main/java/it/luzzetti/justdrink/backoffice/infrastructure/input/rest/mappers/OverruleWebMapper.java
+++ backoffice-service/backoffice-infrastructure/backoffice-input-rest-adapter/src/main/java/it/luzzetti/justdrink/backoffice/infrastructure/input/rest/mappers/OverruleWebMapper.java
@@ -13,21 +13,7 @@ import org.mapstruct.ObjectFactory;
 public interface OverruleWebMapper {
 
   @Mapping(target = "overruleType", ignore = true)
-  OverruleResource toResource(Overrule domain);
-
-  /*
-   * Il metodo qua sopra, mappa come al solito, MA...ma...OverruleResource è una classe astratta.
-   * E quindi? chiederete voi.
-   * Come fa a decidere quale implementazione della classe astratta deve istanziare papà? Diccelo!
-   * Con l'ObjectFactory qua sotto.
-   * Il metodo qua sotto prende l'oggetto di dominio, e fa una scelta di quale classe istanziare
-   * in base a quello che gli diciamo noi.
-   *
-   * Ora, mappandolo con ObjectFactory, si limita ad ISTANZIARLO, poi chiama comunque il mapper
-   * qua sopra per concludere l'implementazione del mapper. (La copia di tutti i campi)
-   *
-   * In realtà è questo sopra che prima di copiare i campi, chiama il factory qua sotto.
-   */
+  OverruleResource toResource(Overrule overrule);
 
   @ObjectFactory
   default OverruleResource factory(Overrule domain) {
@@ -40,7 +26,10 @@ public interface OverruleWebMapper {
     } else {
       OpeningOverruleResource openingOverruleResource = new OpeningOverruleResource();
       openingOverruleResource.setOverruleType(OverruleType.OPENING);
+      openingOverruleResource.setAlternativeOpenTime(domain.getAlternativeOpenTime());
+      openingOverruleResource.setAlternativeCloseTime(domain.getAlternativeCloseTime());
       return openingOverruleResource;
     }
+
   }
 }
