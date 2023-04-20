128dbbfc0268fb44ccef6b6c8b58be44327170ac
Simone Fiani <simone.fiani@outlook.com>
2023-04-20 17:47:20 +0200

    complete (maybe) cuisine's implementation

--- backoffice-service/backoffice-infrastructure/backoffice-input-rest-adapter/src/main/java/it/luzzetti/justdrink/backoffice/infrastructure/input/rest/mappers/CuisineWebMapper.java
+++ backoffice-service/backoffice-infrastructure/backoffice-input-rest-adapter/src/main/java/it/luzzetti/justdrink/backoffice/infrastructure/input/rest/mappers/CuisineWebMapper.java
@@ -1,11 +1,7 @@
 package it.luzzetti.justdrink.backoffice.infrastructure.input.rest.mappers;
 
-import it.luzzetti.justdrink.backoffice.domain.aggregates.menu.MenuSection;
-import it.luzzetti.justdrink.backoffice.domain.shared.typed_ids.MenuSectionId;
 import it.luzzetti.justdrink.backoffice.domain.vo.Cuisine;
-import it.luzzetti.justdrink.backoffice.infrastructure.input.rest.adapters.menu.dto.MenuSectionResource;
 import it.luzzetti.justdrink.backoffice.infrastructure.input.rest.adapters.restaurant.dto.CuisineResource;
-import java.util.UUID;
 import org.mapstruct.Mapper;
 
 @Mapper
