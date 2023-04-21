52521ed7152f7d527cc652bd513b35c315cac0ba
Simone Fiani <simone.fiani@outlook.com>
2023-04-21 16:24:08 +0200

    fixed RestaurantTest

--- backoffice-service/backoffice-domain/src/test/java/it/luzzetti/justdrink/backoffice/domain/aggregates/restaurant/RestaurantTest.java
+++ backoffice-service/backoffice-domain/src/test/java/it/luzzetti/justdrink/backoffice/domain/aggregates/restaurant/RestaurantTest.java
@@ -2,6 +2,7 @@ package it.luzzetti.justdrink.backoffice.domain.aggregates.restaurant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import it.luzzetti.justdrink.backoffice.domain.aggregates.restaurant.Restaurant.RestaurantBuilder;
 import it.luzzetti.justdrink.backoffice.domain.shared.exceptions.ApplicationException;
 import it.luzzetti.justdrink.backoffice.domain.shared.exceptions.ElementNotFoundException;
 import it.luzzetti.justdrink.backoffice.domain.shared.typed_ids.RestaurantId;
@@ -12,69 +13,56 @@ import it.luzzetti.justdrink.backoffice.domain.vo.Coordinates.Longitude;
 import it.luzzetti.justdrink.backoffice.domain.vo.Cuisine;
 import java.util.Set;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RestaurantTest {
 
-  @Test
-  @DisplayName("Restaurant Creating - No id Error")
-  void whenCreatingANewRestaurantWithoutId_throwsIllegalArgumentException() {
-    var theRestaurantBuilder =
+  private RestaurantBuilder theRestaurantBuilder;
+
+  @BeforeEach
+  void init() {
+    theRestaurantBuilder =
         Restaurant.builder()
-            .id(null)
+            .id(RestaurantId.from(UUID.randomUUID()))
             .address(
                 Address.builder()
                     .displayName("via pippo")
                     .coordinates(Coordinates.of(Latitude.of(0.0), Longitude.of(0.0)))
                     .build())
             .name("Fagiano's restaurant");
+  }
+
+  @Test
+  @DisplayName("Restaurant Creating - No id Error")
+  void whenCreatingANewRestaurantWithoutId_thenThrowsApplicationException() {
+    theRestaurantBuilder.id(null);
     assertThrows(ApplicationException.class, theRestaurantBuilder::build);
   }
 
   @Test
   @DisplayName("Restaurant Creating - No name Error")
-  void whenCreatingANewRestaurantWithoutName_throwsIllegalArgumentException() {
-    var theRestaurantBuilder =
-        Restaurant.builder()
-            .id(RestaurantId.from(UUID.randomUUID()))
-            .address(
-                Address.builder()
-                    .displayName("via pippo")
-                    .coordinates(Coordinates.of(Latitude.of(0.0), Longitude.of(0.0)))
-                    .build())
-            .name(null);
+  void whenCreatingANewRestaurantWithoutName_thenThrowsApplicationException() {
+    theRestaurantBuilder.name(null);
     assertThrows(ApplicationException.class, theRestaurantBuilder::build);
   }
 
   @Test
-  @DisplayName("Restaurant Creating - No coordinates Error")
-  void whenCreatingANewRestaurantWithoutCoordinates_throwsIllegalArgumentException() {
-    var theRestaurantBuilder =
-        Restaurant.builder()
-            .id(RestaurantId.from(UUID.randomUUID()))
-            .address(null)
-            .name("Fagiano's restaurant");
+  @DisplayName("Restaurant Creating - No address Error")
+  void whenCreatingANewRestaurantWithoutAddress_thenThrowsApplicationException() {
+    theRestaurantBuilder.address(null);
     assertThrows(ApplicationException.class, theRestaurantBuilder::build);
   }
 
   @Test
   @DisplayName("Add Cuisine to Restaurant - Same Cuisine add")
-  void whenAddingCuisineWithTheSameName_throwsIllegalArgumentException() {
-    Restaurant theRestaurant =
-        Restaurant.builder()
-            .id(RestaurantId.from(UUID.randomUUID()))
-            .address(
-                Address.builder()
-                    .displayName("via pippo")
-                    .coordinates(Coordinates.of(Latitude.of(0.0), Longitude.of(0.0)))
-                    .build())
-            .name("Fagiano's restaurant")
-            .build();
-
+  void whenAddingCuisineWithTheSameName_thenThrowsApplicationException() {
+    Restaurant theRestaurant = theRestaurantBuilder.build();
     Cuisine theSameCuisine = Cuisine.of("Giapponese");
 
     theRestaurant.addCuisine(theSameCuisine);
+
     assertThrows(ApplicationException.class, () -> theRestaurant.addCuisine(theSameCuisine));
   }
 
@@ -85,25 +73,8 @@ class RestaurantTest {
     Cuisine japanCuisine = Cuisine.of("Giapponese");
     Cuisine italianCuisine = Cuisine.of("Italiano");
 
-    Restaurant theRestaurant =
-        Restaurant.builder()
-            .id(RestaurantId.from(UUID.randomUUID()))
-            .address(
-                Address.builder()
-                    .displayName("via pippo")
-                    .coordinates(Coordinates.of(Latitude.of(0.0), Longitude.of(0.0)))
-                    .build())
-            .name("Fagiano's restaurant")
-            .cuisines(Set.of(japanCuisine))
-            .build();
+    Restaurant theRestaurant = theRestaurantBuilder.cuisines(Set.of(japanCuisine)).build();
 
     assertThrows(ElementNotFoundException.class, () -> theRestaurant.removeCuisine(italianCuisine));
   }
-
-  /*
-   * TODO:
-   * Invece di ricreare ogni volta un ristorante, scrivi il codice una volta sola utilizzando la
-   * @BeforeEach
-   */
-
 }
