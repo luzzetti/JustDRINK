3ca072b500e6847d334e875b5317135439137d3c
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-05-05 19:38:47 +0200

    scaffolding creato

---
+++ backoffice-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ 
+<header>
+<Component></Component>
+</header>
+
+</template>
+
+<script>
+import Component from './components/Component.vue';
+
+
+export default{
+  name: "App",
+  components: {
+    Component
+},
+  data() {
+    return {
+    
+    }
+  },
+
+}
+</script>
---
+++ backoffice-vue/src/components/Component.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+ciao
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Component'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
