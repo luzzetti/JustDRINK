211f2abcf9db82b68f9dd4328dfbf700f1569023
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-05-05 23:44:02 +0200

    scaffolding corretto

---
+++ backoffice-ui-vue/src/components/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-btn prepend-icon="fa fa-home" variant="outlined">
+        Button
+    </v-btn>
+</template>
+
+<script>
+export default {
+name:'test',
+}
+</script>
+
+<style lang="scss" scoped></style>
