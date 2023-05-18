2de3225ee46e50916abdb53d705124a4f0800b28
Christian Luzzetti <christian.luzzetti@gmail.com>
2023-05-18 20:03:07 +0200

    Basic configuration on Customer Service

--- backoffice-service/backoffice-bootloader/src/main/java/it/luzzetti/justdrink/backoffice/BackofficeAppLauncher.java
+++ backoffice-service/backoffice-bootloader/src/main/java/it/luzzetti/justdrink/backoffice/BackofficeAppLauncher.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppLauncher {
+public class BackofficeAppLauncher {
   public static void main(String[] args) {
-    SpringApplication.run(AppLauncher.class, args);
+    SpringApplication.run(BackofficeAppLauncher.class, args);
   }
 
 }
--- customer-service/customer-bootloader/src/main/java/it/luzzetti/justdrink/customer/CustomerAppLauncher.java
+++ customer-service/customer-bootloader/src/main/java/it/luzzetti/justdrink/customer/CustomerAppLauncher.java
@@ -1,11 +1,11 @@
-package it.luzzetti.justdrink;
+package it.luzzetti.justdrink.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppLauncher {
+public class CustomerAppLauncher {
   public static void main(String[] args) {
-    SpringApplication.run(AppLauncher.class, args);
+    SpringApplication.run(CustomerAppLauncher.class, args);
   }
 }
